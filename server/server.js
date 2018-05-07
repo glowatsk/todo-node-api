d8ee3ea77d7028586f58caabde751060d4b395b3
glowatsk <glowatsk@gmail.com>
2018-05-07 12:16:32 -0600

    Added GET /todos

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
-    })
+    });
+
     todo.save().then(((doc) => {
         res.send(doc);
     }), (e) => {
@@ -23,6 +24,14 @@ app.post('/todos', (req, res) => {
 });
 
 
+//Send Get Request to fetch all Todos, returns an object.
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
