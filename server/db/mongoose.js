845216a5ebbc85f6e393dc90fa143f4a911b18f0
glowatsk <glowatsk@gmail.com>
2018-05-10 11:50:54 -0600

    Added environment ports for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //connect to mongodb database
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
