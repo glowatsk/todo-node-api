5b654f67b4bd3031a535271f108a0863f2a281ab
glowatsk <glowatsk@gmail.com>
2018-05-24 12:58:33 -0600

    Added POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
-
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 //create new user mongoose model
 var User = mongoose.model('User', {
@@ -7,7 +7,29 @@ var User = mongoose.model('User', {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }});
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '${VALUE} is not a valid email'    
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        }, 
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
 
 module.exports = {User};
