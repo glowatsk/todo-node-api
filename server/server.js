b5ca10dcf5e680ee5ac3767ac288094b2c4e70a2
glowatsk <glowatsk@gmail.com>
2018-06-11 18:29:28 +0000

    Added DELETE and TESTS /users/me/token

--- server/server.js
+++ server/server.js
@@ -146,6 +146,17 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+
+// Delete route
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }).catch(() => {
+        res.status(400).send();
+    })
+});
+
 // This gets called with authenticate middleware
 
 app.get('/users/me', authenticate, (req, res) => {
